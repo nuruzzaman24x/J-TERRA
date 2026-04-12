fd36422411e6292d7f11464dc8b06ee79e904a78
nuruzzaman24x <126858702+nuruzzaman24x@users.noreply.github.com>
2026-04-12 16:07:06 +0600

    Update entrypoint.sh

--- T-Scripts/entrypoint.sh
+++ T-Scripts/entrypoint.sh
@@ -5,4 +5,9 @@ sudo chmod 666 /var/run/docker.sock
 sudo systemctl start docker
 sudo usermod -aG docker ubuntu
 sudo systemctl restart docker
+sudo apt install maven -y
+git clone https://github.com/nuruzzaman24x/jpetstore-6.git
+cd /jpetstore-6
+mv package
+docker build -t petsore .
 docker run -d -p 8080:8080 adijaiswal/petsore
