734de746b9e83f48fc5b47bd4d41856b368db5f4
nuruzzaman24x <126858702+nuruzzaman24x@users.noreply.github.com>
2026-04-12 16:08:32 +0600

    Comment out Maven and jpetstore build steps
    Comment out Maven installation and related commands for jpetstore.

--- T-Scripts/entrypoint.sh
+++ T-Scripts/entrypoint.sh
@@ -5,9 +5,10 @@ sudo chmod 666 /var/run/docker.sock
 sudo systemctl start docker
 sudo usermod -aG docker ubuntu
 sudo systemctl restart docker
-sudo apt install maven -y
-git clone https://github.com/nuruzzaman24x/jpetstore-6.git
-cd /jpetstore-6
-mv package
-docker build -t petsore .
+# sudo apt install maven -y
+# git clone https://github.com/nuruzzaman24x/jpetstore-6.git
+# cd /jpetstore-6
+# mv package
+# docker build -t petsore .
+# if you do not want to make image, then run readymade image
 docker run -d -p 8080:8080 adijaiswal/petsore
