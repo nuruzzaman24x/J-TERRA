ea4ca63d0327a395b899f9b4245e6d0287b1f7d2
nuruzzaman24x <126858702+nuruzzaman24x@users.noreply.github.com>
2026-04-13 15:25:02 +0600

    Update main.tf

--- T-Scripts/main.tf
+++ T-Scripts/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
   access_key= "AKIA6JW3O77PWZBMZZBN"
-  secret_key= "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"
+  secret_key= "xxxxxxxxxxxxxxxxxxxxxxxx"
   
   
 }
